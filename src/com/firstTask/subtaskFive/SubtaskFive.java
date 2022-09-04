618d37a4bdbb5d5890c3ce048be018a4308ee0b2
Vladislav Nasevich <nasevich.vlasd.03@gmail.com>
2022-09-04 17:12:40 +0300

    Completed subtask five.

---
+++ src/com/firstTask/subtaskFive/SubtaskFive.java
@@ -0,0 +1,10 @@
+package com.firstTask.subtaskFive;
+
+public class SubtaskFive {
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++) {
+            System.out.print(((i != 1) ? "1/" : "") + i + " ");
+        }
+    }
+}
